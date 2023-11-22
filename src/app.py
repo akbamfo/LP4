de161abfff3026839dfdd3e1f30ab8bcd12d8cab
akbamfo <bamfoak@gmail.com>
2023-11-22 05:04:50 +0000

    LP4 Project update

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
  
 st.write("""
-# My first app is amazing
-Hello *ewiase world!*
+# Customer Churn Prediction
+## This app was developed for the sole purpose to predict if given certain inputs, an *existing customer* would likely *churn* or exit
+
 """)
+st.text_input('Enter your name')
+st.number_input('Enter your age')
+st.text_input('Enter specie type ')
+st.number_input('Enter which category')
+st.text_input('Enter some text')
+st.number_input('Enter a number')
+st.button('Hit me')
