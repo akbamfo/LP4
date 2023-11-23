6fda07e779abed286324afeb742aee989523d175
akbamfo <bamfoak@gmail.com>
2023-11-23 01:11:00 +0000

    LP4 Project app.py  update

--- src/app.py
+++ src/app.py
@@ -1,14 +1,28 @@
 import streamlit as st
- 
+import pandas as pd
+
 st.write("""
 # Customer Churn Prediction
-## This app was developed for the sole purpose to predict if given certain inputs, an *existing customer* would likely *churn* or exit
-
 """)
-st.text_input('Enter your name')
-st.number_input('Enter your age')
-st.text_input('Enter specie type ')
-st.number_input('Enter which category')
-st.text_input('Enter some text')
-st.number_input('Enter a number')
-st.button('Hit me')
+st.image('src\\image1.jpg')
+st.write("""
+## This app was developed for the sole purpose to predict if given certain inputs, an *existing customer* would likely *churn* or exit
+""")
+
+# Input data
+a = st.text_input('Enter your name')
+b = st.number_input('Enter your age')
+c = st.text_input('Enter specie type ')
+d = st.number_input('Enter which category')
+e = st.text_input('Enter some text')
+f = st.number_input('Enter a number')
+
+# Prediction Button
+if st.button('Hit me'):
+    df = pd.DataFrame(
+            {
+            " ":[a], " ":[b], " ":[c], " ":[d], " ":[e], " ":[f],
+            }
+    )
+    print(f"[info] Input data as dataframe:\n {df.to_text()}")
+    st.text(f"Thank you, your prediction is: '{''}'")
